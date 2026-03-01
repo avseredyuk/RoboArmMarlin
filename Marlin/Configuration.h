c079227de6efee0f5ad05bbdde061af969595dd5
avseredyuk <a.v.seredyuk@gmail.com>
2026-03-01 12:16:19 +0200

    reduce Z feedrate, disable cold extrusion prevention, other

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -846,7 +846,7 @@
  *
  * *** IT IS HIGHLY RECOMMENDED TO LEAVE THIS OPTION ENABLED! ***
  */
-#define PREVENT_COLD_EXTRUSION
+// #define PREVENT_COLD_EXTRUSION
 #define EXTRUDE_MINTEMP 170
 
 /**
@@ -1248,7 +1248,7 @@
  * Override with M203
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 300, 300, 5, 5, 5 }
+#define DEFAULT_MAX_FEEDRATE          { 300, 300, 3, 5, 5 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
